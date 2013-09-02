38c652cc19bb0c52db7af531f19f491055f5bd97
Tom Christie <tom@tomchristie.com>
2013-09-02 15:53:57 +0100

    Flesh out example

--- example/example/settings.py
+++ example/example/settings.py
@@ -72,6 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    'statics',
 )
 
 # List of finder classes that know how to find static files in
