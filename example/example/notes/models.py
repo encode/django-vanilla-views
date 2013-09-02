e376221d438f414d91a38be267daa08d888070fa
Tom Christie <tom@tomchristie.com>
2013-09-02 15:38:20 +0100

    Add minimal working example.

--- example/example/notes/models.py
+++ example/example/notes/models.py
@@ -1,6 +1,14 @@
+# coding=utf-8
 from django.db import models
 
 class Note(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     message = models.CharField(max_length=100)
     complete = models.BooleanField(default=False)
+
+    @property
+    def checkbox_character(self):
+        return '☑' if self.complete else '☐'
+
+    class Meta:
+        ordering = ('-created',)
--- example/example/urls.py
+++ example/example/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
-from vanilla import CreateView, ListView
 from example.notes.models import Note
 
-urlpatterns = patterns('',
-    url(r'^$', ListView.as_view(model=Note), name='note_list'),
-    url(r'^create$', CreateView.as_view(model=Note), name='note_create'),
+urlpatterns = patterns('example.notes.views',
+    url(r'^$', 'list_notes', name='list_notes'),
+    url(r'^create/$', 'create_note', name='create_note'),
+    url(r'^edit/(?P<pk>\d+)/$', 'edit_note', name='edit_note'),
+    url(r'^delete/(?P<pk>\d+)/$', 'delete_note', name='delete_note'),
 )
