4121e366166280bde09baa4cb65c642669073698
Tom Christie <tom@tomchristie.com>
2013-09-24 10:31:32 +0100

    Use style of `.as_view()` in URL conf for the example.

--- example/example/notes/views.py
+++ example/example/notes/views.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import reverse_lazy
 from example.notes.models import Note
 from vanilla import CreateView, DeleteView, ListView, UpdateView
 
+
 class ListNotes(ListView):
     model = Note
 
@@ -19,9 +20,3 @@ class EditNote(UpdateView):
 class DeleteNote(DeleteView):
     model = Note
     success_url = reverse_lazy('list_notes')
-
-
-list_notes = ListNotes.as_view()
-create_note = CreateNote.as_view()
-edit_note = EditNote.as_view()
-delete_note = DeleteNote.as_view()
--- example/example/urls.py
+++ example/example/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from example.notes.models import Note
+from example.notes.views import ListNotes, CreateNote, EditNote, DeleteNote
 
-urlpatterns = patterns('example.notes.views',
-    url(r'^$', 'list_notes', name='list_notes'),
-    url(r'^create/$', 'create_note', name='create_note'),
-    url(r'^edit/(?P<pk>\d+)/$', 'edit_note', name='edit_note'),
-    url(r'^delete/(?P<pk>\d+)/$', 'delete_note', name='delete_note'),
+urlpatterns = patterns('',
+    url(r'^$', ListNotes.as_view(), name='list_notes'),
+    url(r'^create/$', CreateNote.as_view(), name='create_note'),
+    url(r'^edit/(?P<pk>\d+)/$', EditNote.as_view(), name='edit_note'),
+    url(r'^delete/(?P<pk>\d+)/$', DeleteNote.as_view(), name='delete_note'),
 )
