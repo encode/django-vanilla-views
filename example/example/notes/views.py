95786effe724cf798e1dbed93a22e942b7b55d1d
Delio Castillo <jangeador@gmail.com>
2015-08-26 14:30:24 -0700

    fixes issue tomchristie/django-vanilla-views#44
    The example app seems to be missing the form_class which is required
    for CreateView and UpdateView.

--- example/example/notes/views.py
+++ example/example/notes/views.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import reverse_lazy
 from example.notes.models import Note
+from example.notes.forms import NoteForm
 from vanilla import CreateView, DeleteView, ListView, UpdateView
 
 
@@ -9,11 +10,13 @@ class ListNotes(ListView):
 
 class CreateNote(CreateView):
     model = Note
+    form_class = NoteForm
     success_url = reverse_lazy('list_notes')
 
 
 class EditNote(UpdateView):
     model = Note
+    form_class = NoteForm
     success_url = reverse_lazy('list_notes')
 
 
