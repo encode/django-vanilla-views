94868151e546f5d59ca9b83863054b2c1e7873b0
Asif Saifuddin Auvi <auvipy@gmail.com>
2016-05-24 00:46:07 +0600

    url codes update

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from example.notes.models import Note
 from example.notes.views import ListNotes, CreateNote, EditNote, DeleteNote
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', ListNotes.as_view(), name='list_notes'),
     url(r'^create/$', CreateNote.as_view(), name='create_note'),
     url(r'^edit/(?P<pk>\d+)/$', EditNote.as_view(), name='edit_note'),
     url(r'^delete/(?P<pk>\d+)/$', DeleteNote.as_view(), name='delete_note'),
-)
+]
