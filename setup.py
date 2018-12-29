a3a13e47f07539cf78eeda5cd7682c24129a6926
Adam Johnson <me@adamj.eu>
2018-12-29 20:39:12 +0000

    Release as universal wheel as well (#78)
    They're wheely great!

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ def get_package_data(package):
 
 
 if sys.argv[-1] == 'publish':
-    os.system("python setup.py sdist upload")
+    os.system("python setup.py sdist bdist_wheel upload")
     args = {'version': get_version(package)}
     print("You probably want to also tag the version now:")
     print("  git tag -a %(version)s -m 'version %(version)s'" % args)
