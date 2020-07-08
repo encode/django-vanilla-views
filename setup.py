803758bdc2895285b35d4072c76160419c06209a
Adam Johnson <me@adamj.eu>
2020-07-08 16:13:39 +0100

    Drop some Python 2 features
    Wheels will no longer be universal, and the Python 2 classifier should not be there.
    
    Missed in #92.

--- setup.py
+++ setup.py
@@ -104,7 +104,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
     ]
